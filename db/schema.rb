ba9dda4ac5d7674f1b7ff7877a76613dd57ad5bb
fodcool <330469102@qq.com>
2017-07-27 09:25:24 +0800

    增加了 商品分类色表

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170726120014) do
+ActiveRecord::Schema.define(version: 20170727012253) do
+
+  create_table "categories", force: true do |t|
+    t.string   "name"
+    t.string   "category_img"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "goods", force: true do |t|
     t.string   "name"
