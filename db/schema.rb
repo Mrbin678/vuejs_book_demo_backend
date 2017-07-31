c6f41adf8463ea9953baad9633ceaf66d69194a6
fodcool <330469102@qq.com>
2017-07-31 19:58:52 +0800

    修改open_id 的字段类型为string

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170731095049) do
+ActiveRecord::Schema.define(version: 20170731115653) do
 
   create_table "buy_goods", force: true do |t|
     t.integer  "good_id"
@@ -31,7 +31,7 @@ ActiveRecord::Schema.define(version: 20170731095049) do
   create_table "customers", force: true do |t|
     t.string   "name"
     t.string   "avatar"
-    t.integer  "open_id"
+    t.string   "open_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
