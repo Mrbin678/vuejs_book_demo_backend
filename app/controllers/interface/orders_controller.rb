0ae38bf217a8030ab468e61283088438fc35e801
fodcool <330469102@qq.com>
2017-08-03 18:58:13 +0800

    修改了 订单显示的接口， 是待付款，还是 待收货。

--- app/controllers/interface/orders_controller.rb
+++ app/controllers/interface/orders_controller.rb
@@ -1,7 +1,17 @@
 # -*- encoding : utf-8 -*-
 class Interface::OrdersController < Interface::ApplicationController
   def get_all_orders
-    orders = Order.all
+    if params[:order_status].present?
+      if params[:order_status] == "true"
+        orders = Order.where("order_status = ?", true).order("created_at desc")
+      elsif params[:order_status] == "false"
+        orders = Order.where("order_status = ?", false).order("created_at desc")
+      end
+    elsif params[:is_dispatch].present?
+      orders = Order.where("is_dispatch= ?", params[:is_dispatch]).order("created_at desc")
+    else
+      orders = Order.all.order("created_at desc")
+    end
 
     render :json => {
       orders: orders.map { |order|
@@ -10,6 +20,7 @@ class Interface::OrdersController < Interface::ApplicationController
           order_id: order.order_id,
           total_cost: order.total_cost,
           order_status: order.order_status,
+          is_dispatch: order.is_dispatch,
           goods: order.buy_goods.map { |buy_good|
             {
             good_name: buy_good.good.name,
