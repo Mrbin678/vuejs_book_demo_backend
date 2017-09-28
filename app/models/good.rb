3cd8a67d4b650377d5e311ec814288d604e74211
文圆承 <330469102@qq.com>
2017-09-28 15:40:45 +0800

    增加了 会员-评论-商品之间的关联关系。

--- app/models/good.rb
+++ app/models/good.rb
@@ -2,6 +2,7 @@ class Good < ActiveRecord::Base
   belongs_to :category
   has_many :goods_photos
   has_many :buy_goods
+  has_many :comments
 
   def show_goods_first_image
     self.goods_photos.first.image_url
--- app/models/member_user.rb
+++ app/models/member_user.rb
@@ -1,2 +1,3 @@
 class MemberUser < ActiveRecord::Base
+  has_many :comments
 end
